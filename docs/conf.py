db428ddfb63d6787ef45e8211cd0462fe49a189c
Florenz <fl@hacker-stueberl.de>
2023-03-02 19:05:56 +0100

    rename folder to "docs" and move sphinx req's there

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
+    'sphinx_rtd_theme'
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
@@ -30,6 +31,24 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+
+# Sphinx RTD theme options. Docs at https://sphinx-rtd-theme.readthedocs.io/en/stable/index.html
+html_theme_options = {
+    # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    # 'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
