1267793a759b4bd3bfc28655a46932392415d060
Florenz <fl@hacker-stueberl.de>
2024-04-30 08:05:12 +0200

    set a default name when called with no args

--- scripts/create-debian.sh
+++ scripts/create-debian.sh
@@ -5,7 +5,12 @@ POOLDIR="/usr/local/var/vmstore"
 #BACKINGDISK="$POOLDIR/debian-11-genericcloud-amd64.qcow2"
 BACKINGDISK="$POOLDIR/debian-11-generic-amd64.qcow2"
 
-VM="$1"
+if [ -z "$1" ]; then
+  VM="debian11"
+else
+  VM="$1"
+fi
+
 DISK="$VM.qcow2"
 CLOUDINITISO="$VM.cloudinit.iso"
 
@@ -13,39 +18,39 @@ echo "instance-id: $VM" > meta-data
 echo "local-hostname: $VM" >> meta-data
 
 # create a cloud-init ISO
-genisoimage -output "$POOLDIR/$CLOUDINITISO" -V cidata \
-  -r -J user-data meta-data
+#genisoimage -output "$POOLDIR/$CLOUDINITISO" -V cidata \
+#  -r -J user-data meta-data
 
 # create new COW volume backed with the cloud image
-virsh vol-create-as \
-  "$POOL" "$DISK" \
-  10G \
-  --allocation 5G \
-  --format qcow2 \
-  --backing-vol "$BACKINGDISK" \
-  --backing-vol-format qcow2
-
-virt-install \
-  --connect=qemu:///system \
-  --name="$VM" \
-  --os-variant debian11 \
-  --ram=1024 \
-  --vcpus=2 \
-  --import \
-  --disk "$POOLDIR/$DISK" \
-  --disk path="$POOLDIR/$CLOUDINITISO",device=cdrom \
-  --virt-type=kvm \
-  --controller usb,model=none \
-  --network bridge=virbr0,model=virtio \
-  --autoconsole none
+#virsh vol-create-as \
+#  "$POOL" "$DISK" \
+#  10G \
+#  --allocation 5G \
+#  --format qcow2 \
+#  --backing-vol "$BACKINGDISK" \
+#  --backing-vol-format qcow2
+#
+#virt-install \
+#  --connect=qemu:///system \
+#  --name="$VM" \
+#  --os-variant debian11 \
+#  --ram=1024 \
+#  --vcpus=2 \
+#  --import \
+#  --disk "$POOLDIR/$DISK" \
+#  --disk path="$POOLDIR/$CLOUDINITISO",device=cdrom \
+#  --virt-type=kvm \
+#  --controller usb,model=none \
+#  --network bridge=virbr0,model=virtio \
+#  --autoconsole none
 
 # kludge time: wir warten einfach 45s wiel wir denken dass der guest-agent dann dienstbereit ist
 TIMER="45"
 
-while [[ "0" -lt "$TIMER" ]]; do
-  /usr/bin/sleep 1
-  let TIMER--
-  echo "noch $TIMER s..."
-done
-
-virsh domifaddr --source agent "$VM" | grep enp | grep ipv4
+#while [[ "0" -lt "$TIMER" ]]; do
+#  /usr/bin/sleep 1
+#  let TIMER--
+#  echo "noch $TIMER s..."
+#done
+#
+#virsh domifaddr --source agent "$VM" | grep enp | grep ipv4
